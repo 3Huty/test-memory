4fd2bd61406c9a4eac095f38e1848fc15e0b0847
3Huty <sawczuk.a@gmail.com>
2018-08-05 22:43:01 +0200

    add index.html and main.js

--- main/main.js
+++ main/main.js
@@ -38,7 +38,7 @@ function saveName() {
     main.className = "sec_begin";
     
     span = document.getElementById("score_name");
-    span.textContent = input.value;
+    itspan.textContent = input.value;
 }
             
             
@@ -233,3 +233,24 @@ function hideCardsAnimationListener(event) {
 	animationsToPlay--;
 }
 
+ (function(d, s, id) {
+		  var js, fjs = d.getElementsByTagName(s)[0];
+		  if (d.getElementById(id)) return;
+		  js = d.createElement(s); js.id = id;
+		  js.src = 'https://connect.facebook.net/pl_PL/sdk.js#xfbml=1&version=v3.1';
+		  fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));
+	
+	
+window.twttr = (function(d, s, id) {
+		  var js, fjs = d.getElementsByTagName(s)[0],
+			t = window.twttr || {};
+		  if (d.getElementById(id)) return t;
+		  js = d.createElement(s);
+		  js.id = id;
+		  js.src = "https://platform.twitter.com/widgets.js";
+		  fjs.parentNode.insertBefore(js, fjs);
+
+		  t._e = [];
+		  t.ready = function(f) {
+	
